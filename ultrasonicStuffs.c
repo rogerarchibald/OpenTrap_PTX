fd7e5d3d20acd566ca7c6249c58a3f5dafb9f38c
rogerarchibald <roger_archibald@hotmail.com>
2016-06-26 18:27:00 -0500

    Have the 40K pulse stuff working, need to work on INT0 code and test hardware

---
+++ ultrasonicStuffs.c
@@ -0,0 +1,9 @@
+//
+//  ultrasonicStuffs.c
+//  Smart_trap_sonic
+//
+//  Created by Roger on 6/25/16.
+//  Copyright © 2016 Roger. All rights reserved.
+//
+
+#include "ultrasonicStuffs.h"
---
+++ ultrasonicStuffs.h
@@ -0,0 +1,14 @@
+//
+//  ultrasonicStuffs.h
+//  Smart_trap_sonic
+//
+//  Created by Roger on 6/25/16.
+//  Copyright © 2016 Roger. All rights reserved.
+//
+
+#ifndef ultrasonicStuffs_h
+#define ultrasonicStuffs_h
+
+#include <stdio.h>
+
+#endif /* ultrasonicStuffs_h */
